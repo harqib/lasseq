0943e7f61a8b60ce1d41e0a9097af4f4f885d33f
harqib <arqibhouda@gmail.com>
2024-05-21 12:10:43 +0100

    edit config.mjs for basepath

--- lasseq/next.config.mjs
+++ lasseq/next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+
+const isProd = process.env.NODE_ENV = 'production';
+
 const nextConfig = {
     // assetPrefix:'https://<Account-name>/<Repository-name>',
+  basePath: isProd ? '/lasseq' : '',
+  distDir: 'out',
   output: "export",
   reactStrictMode: true,
 };
